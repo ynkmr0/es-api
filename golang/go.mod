47e54fc2c2b4a9bf1f0a2af78f756c63ac3c6a20
Yuta Nakamura <yutanakamura@YutanoMacBook-puro.local>
2020-07-14 17:22:06 +0900

    Update connect es

--- golang/go.mod
+++ golang/go.mod
@@ -2,4 +2,11 @@ module github.com/ynkmr/es-api
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.16
+require (
+	github.com/elastic/go-elasticsearch v0.0.0
+	github.com/elastic/go-elasticsearch/v7 v7.8.0
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/olivere/elastic v6.2.33+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- golang/main.go
+++ golang/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/olivere/elastic"
 )
+
+const esurl = "http://es01:9200"
 
 func main() {
 	// Echo instance
@@ -17,6 +23,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/es", es)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":80"))
@@ -25,4 +32,20 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func es(c echo.Context) error {
+
+	client, err := elastic.NewClient(elastic.SetURL(esurl))
+	if err != nil {
+		// Handle error
+	}
+	fmt.Print("bb")
+	info, code, err := client.Ping(esurl).Do(context.Background())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	m := "Elasticsearch returned with code " + strconv.Itoa(code) + " and version " + info.Version.Number + "\n"
+	return c.String(http.StatusOK, m)
 }
